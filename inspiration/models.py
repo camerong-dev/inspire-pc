4c7ec944e016af4e10c09f54856df9f39c38b31a
camerong-dev <>
2023-05-17 20:18:42 +0000

    Add aditional images to db model

--- inspiration/models.py
+++ inspiration/models.py
@@ -7,6 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=150, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pc_builds')
     feature_image = CloudinaryField('image', default='placeholder')
+    secondary_image = CloudinaryField('image', default='placeholder')
+    tertiary_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     cpu_content = models.TextField()
     gpu_content = models.TextField()
