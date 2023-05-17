02b53f69efa2193023a9d21d7168bc1659a998fb
camerong-dev <>
2023-05-17 20:53:47 +0000

    Add more fields to db model

--- inspiration/models.py
+++ inspiration/models.py
@@ -10,8 +10,15 @@ class Post(models.Model):
     secondary_image = CloudinaryField('image', default='placeholder')
     tertiary_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
-    cpu_content = models.TextField()
-    gpu_content = models.TextField()
+    cpu_content = models.CharField(max_length=150, default='CPU')
+    cpu_cooler_content = models.CharField(max_length=150, default='CPU-Cooler')
+    motherboard_content = models.CharField(max_length=150, default='Motherboard')
+    ram_content = models.CharField(max_length=200, default='RAM')
+    storage_content = models.CharField(max_length=250, default='Storage')
+    gpu_content = models.CharField(max_length=150,  default='GPU')
+    psu_content = models.CharField(max_length=150, default='PSU')
+    case_content = models.CharField(max_length=150, default='Case')
+    fans_content = models.CharField(max_length=150, default='Fans')
     likes = models.ManyToManyField(User, related_name='pc_likes', blank=True)
 
 
