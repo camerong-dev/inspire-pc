819ca9873b516534babfda786a5062c26030e429
camerong-dev <>
2023-06-01 19:19:54 +0000

    Link base.html to css

--- inspiration/forms.py
+++ inspiration/forms.py
@@ -5,21 +5,20 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'feature_image', 'secondary_image', 'tertiary_image', 'cpu_content', 'cpu_cooler_content', 'motherboard_content', 'ram_content', 'storage_content', 'gpu_content', 'psu_content', 'case_content', 'fans_content')
+        fields = ('title', 'feature_image', 'secondary_image', 'tertiary_image', 'cpu_content', 'cpu_cooler_content', 'motherboard_content', 'ram_content', 'storage_content', 'gpu_content', 'psu_content', 'case_content', 'fans_content')
 
         widget = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            #'author': forms.TextInput(attrs={'class': 'form-control'}),
-            'feature_image': forms.FileInput(attrs={'class': 'form-control'}),
-            'secondary_image': forms.FileInput(attrs={'class': 'form-control'}),
-            'tertiary_image': forms.FileInput(attrs={'class': 'form-control'}),
-            'cpu_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'cpu_cooler_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'motherboard_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'ram_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'storage_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'gpu_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'psu_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'case_content': forms.TextInput(attrs={'class': 'form-control'}),
-            'fans_content': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control item', 'placeholder': 'Name of your PC'}),
+            'feature_image': forms.FileInput(attrs={'class': 'form-control item'}),
+            'secondary_image': forms.FileInput(attrs={'class': 'form-control item'}),
+            'tertiary_image': forms.FileInput(attrs={'class': 'form-control item'}),
+            'cpu_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'cpu_cooler_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'motherboard_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'ram_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'storage_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'gpu_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'psu_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'case_content': forms.TextInput(attrs={'class': 'form-control item'}),
+            'fans_content': forms.TextInput(attrs={'class': 'form-control item'}),
         }
