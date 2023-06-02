b4fb381f12b4f19ef75fc877f0e718180d221e50
camerong-dev <>
2023-06-02 13:08:08 +0000

    Added Static to URL Patterns

--- inspiration/urls.py
+++ inspiration/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 from .views import Home, AddPC, PCDetail
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,3 +10,5 @@ urlpatterns = [
     path('pc-build/<int:pk>', PCDetail.as_view(), name="pc-details"),
     path('add_pc/', AddPC.as_view(), name='add_pc'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
