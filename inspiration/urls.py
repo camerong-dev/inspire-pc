239ac0ec10c5222af7c73a310de16503af462a24
camerong-dev <>
2023-06-06 21:28:01 +0000

    Render results on gpu or cpu manufacturer

--- inspiration/urls.py
+++ inspiration/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
-from .views import Home, AddPC, PCDetail, CpuManView
+from .views import Home, AddPC, PCDetail, CpuManView, GpuManView
 
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('pc-build/<int:pk>', PCDetail.as_view(), name="pc-details"),
     path('add_pc/', AddPC.as_view(), name='add_pc'),
-    path('cpu_manufacturer/<str:cpu_man>/', CpuManView, name='cpuman')
+    path('cpu_manufacturer/<str:cpu_man>/', CpuManView, name='cpuman'),
+    path('gpu_manufacturer/<str:gpu_man>/', GpuManView, name='gpuman'),
 ]
--- inspiration/views.py
+++ inspiration/views.py
@@ -5,14 +5,19 @@ from .forms import PostForm
 from django.core.paginator import Paginator
 
 
+def CpuManView(request, cpu_man):
+    cpu_man_posts = Post.objects.filter(cpu_manufacturer=cpu_man)
+    return render(request, 'cpu_man.html', {'cpu_man': cpu_man, 'cpu_man_posts': cpu_man_posts})
+
+def GpuManView(request, gpu_man):
+    gpu_man_posts = Post.objects.filter(gpu_manufacturer=gpu_man)
+    return render(request, 'gpu_man.html', {'gpu_man': gpu_man, 'gpu_man_posts': gpu_man_posts})
+
+
 class Home(ListView):
     model = Post
     template_name = 'home.html'
     paginate_by = 9
-
-
-def CpuManView(request, cpu_man):
-    return render(request, cpu_man.html)
 
 
 class PCDetail(DetailView):
